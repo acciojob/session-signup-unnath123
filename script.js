5b242e804609d9f2deb435012e06054955c43624
unnath123 <64188873+unnath123@users.noreply.github.com>
2023-09-22 23:22:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+const btn = document.getElementById("submit");
+
+const nam = document.getElementById("name")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const Cfmpass = document.getElementById("confirm-password")
+
+
+btn.addEventListener("click",(event)=>{
+    // event.preventDefault();
+    const name1 = nam.value;
+    const email1 = email.value;
+    const pass = password.value;
+    const cfp = Cfmpass.value;
+
+    if(pass === cfp){
+        sessionStorage.setItem("name",name1)
+        sessionStorage.setItem("email",email1)
+        sessionStorage.setItem("password",pass)
+        alert("Sign up successful!")
+    }
+    else{
+        alert("Passwords do not match")
+    }
+    document.reload()
+})
